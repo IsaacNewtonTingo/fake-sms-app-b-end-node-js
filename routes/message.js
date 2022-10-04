90d09ce35f194f93469af5915b6b594bc884b3c0
Isaac  Tingo <newtone480@gmail.com>
2022-10-04 18:53:28 +0300

    Date

--- routes/message.js
+++ routes/message.js
@@ -10,8 +10,7 @@ router.post("/post-message", async (req, res) => {
     amount: amount,
     account: account,
     balance: 1230 - amount,
-    createdAt: Date.now(),
-    // createdAt: Date.now() + 10800000,
+    createdAt: Date.now() + 10800000,
   });
 
   await newMessage
